0e428266ffdb86da99fc75a2e31dd6f5f91a19d2
Andy Lindeman <andy@highgroove.com>
2011-02-26 10:17:18 -0600

    Whitespace cleanups

--- lib/decor.rb
+++ lib/decor.rb
@@ -1,136 +1,136 @@
 # Decor provides a simple way to define multiple representations of an object.
-# 
+#
 # This is useful for when you want to retain multiple versions of your objects
 # while providing a consistent interface between versions.
-# 
+#
 # For example:
-# 
+#
 #     class Company < ActiveRecord::Base
 #       include Decor
-#       
+#
 #       # the first version, customers are dependent on certain artifacts
 #       # like industry_id and industry
 #       version "v1" do
 #         INDUSTRIES = {1 => "Farm",
 #                       2 => "Software"}
-#         
+#
 #         def industry
 #           INDUSTRIES[industry_id]
 #         end
-#         
+#
 #         def as_json(*_)
 #           super(:only     => [:id, :name, :industry_id],
 #                 :methods  => [:industry])
 #         end
 #       end
-#       
+#
 #       # switch to using industry standard codes, cleans up the name
 #       version "v2" do
 #         SIC_CODES = {...}
-#         
+#
 #         # use the cleaned name for the name instead
 #         def name
 #           name_cleaned
 #         end
-#         
+#
 #         def industry
 #           SIC_CODES[sic_code]
 #         end
-#         
+#
 #         def as_json(*_)
 #           super(:only     => [:id, :name, :sic_code],
 #                 :methods  => [:industry])
 #         end
 #       end
-#       
+#
 #     end
-# 
+#
 # In our API, for instance, we can then define a single endpoint for both
 # versions:
-# 
+#
 #     get "/api/:version/companies/:id.json" do
 #       Company.find(params[:id]).for(version).to_json
 #     end
-# 
+#
 # This helps us keep our models fat and our "controllers" skinny. This also
 # helps unit testing the versions of your API.
-# 
+#
 # Further details can be found on [Github](https://github.com/mtodd/decor/).
-# 
+#
 module Decor
-  
+
   # Decor is a mixin. Include it into your class and then use the `version`
   # class methods in the class body to define your versions, then use the
   # `for` instance method on your objects to use a specific version.
-  # 
+  #
   # For example:
-  # 
+  #
   #     class Model
   #       include Decor
-  #       
+  #
   #       version "v1" do
   #         # implement specifics for this version here
   #       end
   #     end
-  # 
+  #
   #     model = Model.new.for("v1")
-  # 
+  #
   def self.included(target)
     target.send(:extend, ClassMethods)
     class << target; attr_accessor :versions; end
     target.versions = {}
   end
-  
+
   module ClassMethods
     # Defines versions of the model's representation.
-    # 
+    #
     # The `version` supplied is a string representation of the version.
     # For example, `"v1"` represents version 1 of this model's representation.
-    # 
+    #
     # The `version` is just a key, however, and any value works. Strings are
     # convenient, especially in the form of `v1` or `v2010-12-09`.
-    # 
+    #
     # If a block is provided, the block is treated as the body of the version's
     # definition.
-    # 
+    #
     # If a hash of `version => version_module` is passed in, we use your module
     # instead of creating our own.
-    # 
+    #
     # For example:
-    # 
+    #
     #     class Model
     #       include Decor
-    #       
+    #
     #       version "v1" do
     #         # ...
     #       end
-    # 
+    #
     #       module V20101209
     #         # ...
     #       end
     #       version "v2010-12-09" => V20101209
-    #       
+    #
     #       # or use classes (for example) as version keys and alias to other
     #       # versions
     #       version AnotherModel  => "v1"
     #       version OtherModel    => V20101209
-    #       
+    #
     #     end
-    # 
+    #
     def version(version, &block)
       case
       # Look up version module if no version block or module specified.
       #     version "v1" #=> #<Module>
       when self.versions.key?(version)
         return self.versions[version]
-        
+
       # Define a new version from the block.
       #     version "v1" { ... }
       when block_given?
         constant = Module.new(&block)
         self.versions[version] = constant
         self
-        
+
       # Set versions from a module, supports as many versions as passed in.
       #     version "v1"        => Version1,
       #             "v2"        => Version2,
@@ -144,60 +144,60 @@ module Decor
             self.versions[module_or_version]
           end
         end
-        
+
       end
       self
     end
   end
-  
+
   # An object will be told to behave like the version specified.
-  # 
+  #
   # Options provide additional values in the context of the verions.
-  # 
+  #
   #     class Model
   #       include Decor
-  #       
+  #
   #       version "v1" do
   #         def versioned?
   #           true
   #         end
   #       end
-  #       
+  #
   #       def versioned?
   #         false
   #       end
   #     end
-  # 
+  #
   #     model = Model.new
   #     model.          versioned?  #=> false
   #     model.for("v1").versioned?  #=> true
-  # 
+  #
   # An optional context can be supplied which will make external resources
   # available for specific functions in your versions. For example:
-  # 
+  #
   #     class User
   #       include Decor
-  #       
+  #
   #       version "v1" do
   #         def display_name
   #           "%s (%s)" % [name, band.display_name]
   #         end
   #       end
   #     end
-  # 
+  #
   #     user = User.find(id).for("v1", :band => external_band)
   #     user.display_name #=> "Dan Auerbach (The Black Keys)"
-  # 
+  #
   # Lastly, it's possible to pass in a `:module` option which will override the
   # module already defined for the version specified (making the `version`
   # passed in almost meaningless).
-  # 
+  #
   #     module Specialized
   #       # special considerations here
   #     end
-  #     
+  #
   #     Model.new.for("v1", :module => Specialized)
-  # 
+  #
   def for(version, options = {})
     version_module = self.version_module_for(version, options)
 
@@ -216,12 +216,12 @@ module Decor
 
     self
   end
-  
+
   # Handles finding the module defined for the `version` specified, or
   # overriding with the `:module` option.
-  # 
+  #
   # See `for` for details.
-  # 
+  #
   def version_module_for(version, options = {})
     return options.delete(:module)      if options.key?(:module)
     return self.class.versions[version] if self.class.versions.key?(version)
--- spec/decor_spec.rb
+++ spec/decor_spec.rb
@@ -5,119 +5,119 @@ require 'models/resource'
 require 'models/company'
 
 describe Decor do
-  
+
   describe "when included in a class" do
-    
+
     describe ".version" do
       it "should be available" do
         Bare.should_not respond_to(:version)
         Bare.send(:include, Decor)
         Bare.should respond_to(:version)
       end
-      
+
       it "should define a collection of versions" do
         Bare.versions.should respond_to(:to_hash)
       end
-      
+
       it "should allow new versions to be defined with a block" do
         Bare.should respond_to(:version)
         Bare.version("v1"){}
         Bare.versions.key?("v1").should be_true
       end
-      
+
       it "should allow new versions to be defined with a module" do
         v2 = Module.new
         Bare.version "v2" => v2
         Bare.versions.key?("v2").should be_true
         Bare.versions["v2"].should == v2
       end
-      
+
       it "should return versions by name when no block or module is given" do
         v3 = Module.new
         Bare.version "v3" => v3
         Bare.version("v3").should == v3
       end
-      
+
       it "should turn a block into a module" do
         Bare.version("v4"){}
         Bare.version("v4").should be_a(Module)
       end
     end
-    
+
     describe "#for" do
       subject{ Bare.new }
-      
+
       it "should decorate the object with the version specified" do
         subject.for("v1").version.should == "v1"
       end
-      
+
       it "should be the instance" do
         subject.for("v1").should be_a(Bare)
         subject.for("v1").should == subject
       end
-      
+
       it "should decorate by extending the version module" do
         subject.for("v1").should be_a(Bare.version("v1"))
       end
-      
+
     end
-    
+
   end
-  
+
   describe "decoration behavior" do
     before{ @version = "v1"; @name = "foo"; @value = 2; @multi = 2; @options = {} }
     subject{ Resource.new(@name, @value, @multi).for(@version, @options) }
-    
+
     it "should delegate to the object" do
       subject.name.should == @name
     end
-    
+
     it "should allow overriding the module for the version" do
       v2 = Module.new
       resource = subject.for("v1", :module => v2)
       resource.should be_a(v2)
     end
-    
+
     describe "v1" do
       before{ @version = "v1" }
-      
+
       it "should delegate to the target for original values" do
         subject.name.should == @name
       end
-      
+
       it "should allow computed values" do
         subject.computed.should == @value * @multi
       end
     end
-    
+
     describe "v2" do
       before{ @version = "v2" }
-      
+
       it "should allow modified versions of methods in the target" do
         subject.name.should == @name.upcase
       end
-      
+
       it "should allow constants in the context of the version" do
         subject.computed.should == @value * @multi * Resource::Version2::MUTLI
       end
-      
+
     end
-    
+
     describe "v2010-12-08" do
       before{ @version = "v2010-12-08" }
-      
+
       it "should keep the name of the alias as the version" do
         subject.version.should == "v2010-12-08"
       end
-      
+
       it "should be the same as its alias" do
         subject.should be_a(subject.class.version("v2"))
       end
     end
-    
+
     describe "v3" do
       before{ @version = "v3"; @options = {:foo => "bar"} }
-      
+
       it "should use values in the context hash as methods" do
         subject.name.should == "%s (%s)" % [@name, @options[:foo]]
       end
@@ -156,7 +156,7 @@ describe Decor do
         subject_from_json['company'].should_not have_key('v1_field')
       end
     end
-    
+
   end
-  
+
 end
